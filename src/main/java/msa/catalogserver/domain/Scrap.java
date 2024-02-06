414f00ef53513d4c0c2b3afb70f117a3f4f13911
kamothi <rhkddlf7911@naver.com>
2024-02-06 22:39:49 +0900

    Init: Scrap 엔티티 추가

---
+++ src/main/java/msa/catalogserver/domain/Scrap.java
@@ -0,0 +1,23 @@
+package msa.catalogserver.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "scrap")
+public class Scrap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="scrap_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String user_id;
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JoinColumn(name = "catalogEntity_id")
+    private CatalogEntity catalog;
+
+}
