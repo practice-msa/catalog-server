aae9650093ace2a254d1d012e53ea26c5c965cb7
kamothi <rhkddlf7911@naver.com>
2024-02-06 20:30:30 +0900

    Init: Brand 엔티티 추가

---
+++ src/main/java/msa/catalogserver/domain/Brand.java
@@ -0,0 +1,27 @@
+package msa.catalogserver.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name= "brand")
+@Getter
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="brand_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String logo;
+
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "brand")
+    private List<CatalogEntity> catalogs;
+}
