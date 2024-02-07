d1e307ac504c6f1842fb7a66cb74a6f9a69888b4
kamothi <rhkddlf7911@naver.com>
2024-02-07 15:10:36 +0900

    Feat: 상품 추가 기능

--- src/main/java/msa/catalogserver/controller/ProductController.java
+++ src/main/java/msa/catalogserver/controller/ProductController.java
@@ -3,13 +3,12 @@ package msa.catalogserver.controller;
 import lombok.RequiredArgsConstructor;
 import msa.catalogserver.domain.Product;
 import msa.catalogserver.service.ProductService;
+import msa.catalogserver.vo.product.RequestCreateProduct;
 import msa.catalogserver.vo.product.ResponseProduct;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +16,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/catalog-service")
 @RequiredArgsConstructor
-public class CatalogController {
+public class ProductController {
     private final Environment env;
-    private final ProductService catalogService;
+    private final ProductService productService;
 
     @RequestMapping("/health_check")
     public String status(){
@@ -27,14 +26,22 @@ public class CatalogController {
         return String.format("catalog service Port %s",env.getProperty("local.server.port"));
     }
 
-    @GetMapping("/catalogs")
-    public ResponseEntity<List<ResponseProduct>> getUsers(){
-        Iterable<Product> catalogList = catalogService.getAllCatalogs();
-        List<ResponseProduct> result = new ArrayList<>();
+//    @GetMapping("/catalogs")
+//    public ResponseEntity<List<ResponseProduct>> getUsers(){
+//        Iterable<Product> catalogList = productService.getAllCatalogs();
+//        List<ResponseProduct> result = new ArrayList<>();
+//
+//        for (Product catalogEntity : catalogList) {
+//            result.add(ResponseProduct.from(catalogEntity));
+//        }
+//        return ResponseEntity.status(HttpStatus.OK).body(result);
+//    }
 
-        for (Product catalogEntity : catalogList) {
-            result.add(ResponseProduct.from(catalogEntity));
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(result);
+    @PostMapping("/catalog")
+    public ResponseEntity<String> createProduct(@RequestBody RequestCreateProduct requestCreateProduct){
+        productService.createProduct(requestCreateProduct);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("성공");
+
     }
 }
