0d896358d2d1a83e90f126c8173e8cad5a30bcfc
kamothi <rhkddlf7911@naver.com>
2024-02-07 15:14:18 +0900

    Feat: 서비스 구현체 구현

--- src/main/java/msa/catalogserver/service/BrandServiceImpl.java
+++ src/main/java/msa/catalogserver/service/BrandServiceImpl.java
@@ -1,2 +1,23 @@
-package msa.catalogserver.service;public class BrandServcieImpl {
+package msa.catalogserver.service;
+
+import lombok.RequiredArgsConstructor;
+import msa.catalogserver.domain.Brand;
+import msa.catalogserver.repository.BrandRepository;
+import msa.catalogserver.vo.brand.RequestCreateBrand;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BrandServiceImpl implements BrandService{
+    private final BrandRepository brandRepository;
+    @Override
+    @Transactional
+    public void registerBrand(RequestCreateBrand requestCreateBrand) {
+        Brand brand = new Brand();
+        brand.setDescription(requestCreateBrand.getDescription());
+        brand.setLogo(requestCreateBrand.getLogo());
+        brand.setName(requestCreateBrand.getName());
+        brandRepository.save(brand);
+    }
 }
