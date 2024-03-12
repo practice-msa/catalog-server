f34fdfa5dc1b50c7efcc3bcc14d7a9312d3723aa
kamothi <rhkddlf7911@naver.com>
2024-03-13 00:19:15 +0900

    Feat: Response format 추가

---
+++ src/main/java/msa/catalogserver/dto/response/ApiResponse.java
@@ -0,0 +1,32 @@
+package msa.catalogserver.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.http.HttpStatus;
+
+public class ApiResponse<T>{
+    private Boolean isSuccess;
+    private T response;
+    private HttpStatus code;
+    private ErrorResponse errorResponse;
+
+    public ApiResponse(Boolean isSuccess, T response, HttpStatus code, ErrorResponse errorResponse) {
+        this.isSuccess = isSuccess;
+        this.response = response;
+        this.code = code;
+        this.errorResponse = errorResponse;
+    }
+
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    public Boolean getIsSuccess() {
+        return isSuccess;
+    }
+
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    public T getResponse() {
+        return response;
+    }
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    public ErrorResponse getErrorResponse() {
+        return errorResponse;
+    }
+}
