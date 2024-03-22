7dd05c6ce1db7af2bfa28fdb2b51aa1175ab1c12
kamothi <rhkddlf7911@naver.com>
2024-03-22 20:50:19 +0900

    Feat: aws S3 설정 및 기능 추가

---
+++ src/main/java/msa/catalogserver/aws/S3Service.java
@@ -0,0 +1,16 @@
+package msa.catalogserver.aws;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+// 여기는 저장, 삭제 구현
+public interface S3Service {
+    // 저장 -> 주소값 반환
+    String upload(MultipartFile file) throws IOException;
+
+    // 삭제
+    String delete(String url);
+
+
+}
---
+++ src/main/java/msa/catalogserver/aws/S3ServiceImpl.java
@@ -0,0 +1,37 @@
+package msa.catalogserver.aws;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3ServiceImpl implements S3Service{
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+    @Override
+    public String upload(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        String fileUrl = "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/" + file;
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+        amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+        return fileUrl;
+    }
+
+    @Override
+    public String delete(String url) {
+        amazonS3Client.deleteObject(bucket, url);
+        String message = "Delete 성공";
+        return message;
+    }
+}
